8dc2181241dba9ac48da94c85ac3ab8e3c80863c
Tanvi Jain <tanvi004@e.ntu.edu.sg>
2026-04-03 00:39:55 +0800

    Change project.py to support tkinter interface

--- project.py
+++ project.py
@@ -1,7 +1,14 @@
-#ASSUMING INTERFACE IS ALR READY and the main function to launch UI is called launch_app (rename if not)
-from interface import launch_app
+# #ASSUMING INTERFACE IS ALR READY and the main function to launch UI is called launch_app (rename if not)
+# from interface import launch_app
 
-if __name__ == "__main__":
-    launch_app()
+# if __name__ == "__main__":
+#     launch_app()
     
+    
+import tkinter as tk
+from interface import QueryAnnotator
+
+root=tk.Tk()
+app=QueryAnnotator(root)
+root.mainloop()
     
